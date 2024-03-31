6e888e8ec320d12fead7afb9e026da97ed03a4f8
Liz Martin-Strong <liz.martin0518@gmail.com>
2024-03-31 12:50:01 -0700

    graph progress

--- belly_button_challenge/StarterCode/static/js/app.js
+++ belly_button_challenge/StarterCode/static/js/app.js
@@ -1,40 +1,61 @@
 // Read in json data file samples.json
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
-
 let bb_data;
 
 // Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-    bb_data = data;
-    console.log(bb_data);
+d3.json(url).then(function(bb_data) {
 
-    // Get needed values for data set.
-    let names = bb_data.names;
+    // Create initial variables for data set.
+    let subject_names = bb_data.names;
     let metadata = bb_data.metadata;
     let samples = bb_data.samples;
         // Loop through sample array to get data
-        function listLoop(samples) {
-            for (let i = 0; i < length.bb_data; i++) {
-            console.log(samples[i]);
-            }
+        function init(){
+            let filteredSamples = samples.filter(samples => samples.id =="940")
+
+            // Create first graph (bar chart)
+            let barGraph = [{
+                 x: filteredSamples.map(item => item.sample_values.slice(0,10).reverse())[0],
+                 y: filteredSamples.map(item => item.otu_ids.slice(0,10).reverse())[0].map(item => 'OTU ${item}'),
+                 text: filteredSamples.map(item => item.otu_labels.slice(0,10).reverse())[0],
+                 type: "bar",
+                 orientation: "h"}];
+            let graphLayout = {
+                margin: {
+                    l: 180,
+                    r: 180,
+                    t: 90,
+                    b: 90,
+                }
+            };
+            Plotly.newPlot("bar", barGraph, graphLayout);
+
+            // Create second graph (bubble chart)
+            let bubbleChart = [{
+                x: filteredSamples.map(item => item.otu_ids)[0],
+                y: filteredSamples.map(item => item.sample_values)[0],
+                mode: 'markers',
+                text: filteredSamples.map(item => item.otu_labels)[0],
+                type: 'scatter',
+                marker: {
+                    size: filteredSamples.map(item => item.sample_values)[0],
+                    color: filteredSamples.map(item => item.otu_ids)[0],
+                }
+            }];
+            let bubble_layout = {
+                xaxis: {title: "OTU ID"}
+            };
+            Plotly.newPlot("bubble", bubbleChart, bubble_layout);
+
+            // Initial demographic card
+            let metadata_info = d3.select("#sample-metadata");
+            let filtered_metadata = metadata.filter(sample => sample.id == "940")[0];
+            Object.entries(filtered_metadata).forEach(([key, v]) => {
+                metadata_info.append("p").text('${key}: ${v}');
+            });
         }
 
-    let otu_ids = samples.id.map(index => index)
 
-    let sampleArray = [names, metadata, samples, otu_ids]
-
-// Create first graph
-function plotMetric(sampleArray, names, metadata){
-        let trace1 = {
-            //x: [<name for data out of my samples>],
-            y: [otu_ids],
-            type: "bar",
-            orientation: "h"
-        };
-    }
-let bbdata = [trace1];
   // Create our second trace
   //let trace2 = {
     //x: [0, 1, 2, 3, 4, 5],
@@ -43,10 +64,10 @@ let bbdata = [trace1];
   //};
   
   // The data array consists of both traces
-  l//et data = [trace1, trace2];
+  //let data = [trace1, trace2];
   
   // Note that we omitted the layout object this time
   // This will use default parameters for the layout
-  Plotly.newPlot("plot", bbdata);
-});
+  //Plotly.newPlot("plot", bbdata);
+//});
 
