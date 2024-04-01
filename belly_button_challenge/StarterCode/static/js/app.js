3b975fd3fb9b654d3a6fa24d9f8a0cf90f7ad623
Liz Martin-Strong <liz.martin0518@gmail.com>
2024-04-01 16:42:09 -0700

    progress; still not right

--- belly_button_challenge/StarterCode/static/js/app.js
+++ belly_button_challenge/StarterCode/static/js/app.js
@@ -1,25 +1,23 @@
 // Read in json data file samples.json
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
-let bb_data;
 
 // Fetch the JSON data and console log it
 d3.json(url).then(function(bb_data) {
-
-    // Create initial variables for data set.
     let subject_names = bb_data.names;
     let metadata = bb_data.metadata;
     let sample_set = bb_data.samples;
-        // Loop through sample array to get data
-        function init(){
+
+        // Create graphs for initial sample
+        function init() {
             let filteredSamples = sample_set.filter(sample => sample.id =="940")
 
             // Create first graph (bar chart)
             let barGraph = [{
-                 x: filteredSamples.map(item => item.sample_values.slice(0,10).reverse())[0],
-                 y: filteredSamples.map(item => item.otu_ids.slice(0,10).reverse())[0].map(item => 'OTU ${item}'),
-                 text: filteredSamples.map(item => item.otu_labels.slice(0,10).reverse())[0],
-                 type: "bar",
-                 orientation: "h"}];
+                x: filteredSamples.map(item => item.sample_values.slice(0,10).reverse())[0],
+                y: filteredSamples.map(item => item.otu_ids.slice(0,10).reverse())[0].map(item => `OTU ${item}`),
+                text: filteredSamples.map(item => item.otu_labels.slice(0,10).reverse())[0],
+                type: 'bar',
+                orientation: 'h'}];
             let graphLayout = {
                 margin: {
                     l: 180,
@@ -48,28 +46,58 @@ d3.json(url).then(function(bb_data) {
             Plotly.newPlot("bubble", bubbleChart, bubble_layout);
 
             // Initial demographic card
-            let metadata_info = d3.select("#sample-metadata");
+            let metaDataInfo = d3.select("#sample-metadata");
             let filtered_metadata = metadata.filter(sample => sample.id == "940")[0];
             Object.entries(filtered_metadata).forEach(([key, v]) => {
-                metadata_info.append("p").text('${key}: ${v}');
+                metaDataInfo.append("p").text(`${key}: ${v}`);
             });
+        }
+
+    // Drop down menu configuration
+    let dropdown = d2.select("#selDataset");
+
+    // Bind the subject names to drop down
+    let options = dropdown.selectAll("options")
+        .data(subject_names)
+        .enter()
+        .append("option");
+     options.text(sub => sub).attr("value", sub => sub);
+
+    //Create function for the option change
+    function optionChanged(){
+        let selectedSubject = d3.select(this).property("value");
+        updateGraphs(selectedSubject);
         };
 
-        // Drop down menu configuration
-        let dropdown = d2.select("#selDataset");
+    // Add event listener for option selection of dropdown
+    dropdown.on("change", optionChanged);
 
-        // Bind the subject names to drop down
-        let options = dropdown.selectAll("options")
-            .data(subjects)
-            .enter()
-            .append("option");
-        options.text(sub => sub).attr("value", sub => sub);
+    // Update charts
+    function updateCharts(selectedSubject) {
+        let filteredSamples = sample_set.filter(sample => sample.id == selectedSubject)
+        
+        // Update bar grpah
+        Plotly.restyle("bar", {
+            x: [filteredSamples.map(item => item.sample_values.slice(0, 10).reverse())[0]],
+            y: [filteredSamples.map(item => item.otu_ids.slice(0, 10).reverse())[0].map(item => `OTU ${item}`)],
+            text: [filteredSamples.map(item => item.otu_labels.slice(0, 10).reverse())[0]]
+        });
 
-        // Create function for the option change
-        function optionSelect(){
-            let selectedSubject = d3.select(this).property("value");
-            updateGraphs(selectedSubject);
-        };
+        // Update bubble chart
+        Plotly.restyle("bubble", {
+            x: [filteredSamples.map(item => item.otu_ids)[0]],
+            y: [filteredSamples.map(item => item.sample_values)[0]],
+            text: [filteredSamples.map(item => item.otu_labels)[0]],
+        });
+        Plotly.restyle("bubble", "marker.size", [filteredSamples.map(item => item.sample_values)[0]]);
+        Plotly.restyle("bubble", "marker.color", [filteredSamples.map(item => item.otu_ids)[0]]);
+        
+        // update demographic card
+        let metaDataInfo = d3.select("#sample-metadata");
+        let filtered_metadata = metadata.filter(sample => sample.id == "940")[0];
+        Object.entries(filtered_metadata).forEach(([key, v]) => {
+            metaDataInfo.append("p").text(`${key}: ${v}`);
+        });
+    }
 init();
 });
-
